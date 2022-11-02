2f405fc4fcee4db190882e22750e52ec2ce7a569
Matthias Stein <m.stein@conterra.de>
2022-11-02 16:36:15 +0100

    Update to map.apps 4.14.0

--- src/main/js/bundles/sample_helloworld/nls/de/bundle.js
+++ src/main/js/bundles/sample_helloworld/nls/de/bundle.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-module.exports = {
+export default {
     bundleName: "Hallo Welt",
     bundleDescription: "Das Modul gibt Hallo aus.",
     hello: "Hallo Welt"
